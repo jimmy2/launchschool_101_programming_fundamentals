2b1d07c8f5763259653401507d83e484fe64820f
James Ferguson <jimmy2289@gmail.com>
2018-05-06 13:29:01 +0900

    Twenty One game with bonus features.

--- lesson_6_slightly_larger_problems/twenty_one.rb
+++ lesson_6_slightly_larger_problems/twenty_one.rb
@@ -4,6 +4,9 @@
 values = %w[A 2 3 4 5 6 7 8 9 10 J Q K]
 suits = %w[S H D C]
 DECK = suits.product(values)
+GAME_MAX = 21
+DEALER_STAY = 17
+WINNING_SCORE = 5
 players_cards = []
 dealers_cards = []
 
@@ -15,20 +18,34 @@ def initialize_deck(deck)
   deck.shuffle
 end
 
-def determine_winner(player, dealer)
-  player_score = calculate_score(player)
-  dealer_score = calculate_score(dealer)
-  if player_score > 21
-    "The dealer won!"
-  elsif dealer_score > 21
-    "You won!"
+def determine_winner(player_score, dealer_score)
+  if player_score > GAME_MAX
+    :dealer
+  elsif dealer_score > GAME_MAX
+    :player
   elsif player_score == dealer_score
-    "Push... You both have the same score (#{player_score})"
+    :tie
   elsif player_score > dealer_score
-    "You won! (#{player_score} to #{dealer_score})"
+    :player
   else
-    "The dealer won! (#{dealer_score} to #{player_score})"
+    :dealer
   end
+end
+
+def print_summary(player_score, dealer_score, winner, scores)
+  puts ""
+  player_score = "Bust!" if player_score > GAME_MAX
+  dealer_score = "Bust!" if dealer_score > GAME_MAX
+  if winner == :player
+    puts "You won! (#{player_score} to #{dealer_score})"
+  elsif winner == :dealer
+    puts "The dealer won! (#{dealer_score} to #{player_score})"
+  else
+    puts "Push... You both have the same score (#{player_score})"
+  end
+  prompt "Game score: "
+  prompt "You #{scores[:player]} : Dealer #{scores[:dealer]}"
+  prompt "#{scores[:tie]} ties."
 end
 
 # rubocop:disable Metrics/MethodLength
@@ -46,7 +63,7 @@ def calculate_score(user)
     end
   end
   total = score.reduce(:+)
-  while total > 21 && ace_count > 0
+  while total > GAME_MAX && ace_count > 0
     total -= 10
     ace_count -= 1
   end
@@ -54,17 +71,17 @@ def calculate_score(user)
 end
 # rubocop:enable Metrics/MethodLength
 
-def busted?(user)
-  calculate_score(user) > 21
+def busted?(score)
+  score > GAME_MAX
 end
 
-def print_deal(player, dealer, hide = true)
+def print_deal(player_cards, dealer_cards, plr_scr, dlr_scr = 0, hide = true)
   if hide
-    puts "Dealer has: #{face_value(dealer[0][1])} and an unknown card"
+    puts "Dealer has: #{face_value(dealer_cards[0][1])} and an unknown card"
   else
-    puts "Dealer has: #{print_cards(dealer)}; (#{calculate_score(dealer)})"
+    puts "Dealer has: #{print_cards(dealer_cards)}; (#{dlr_scr})"
   end
-  puts "You have: #{print_cards(player)}; (#{calculate_score(player)})"
+  puts "You have: #{print_cards(player_cards)}; (#{plr_scr})"
 end
 
 def print_cards(user)
@@ -85,44 +102,63 @@ def face_value(card)
 end
 
 # Start game
-deck = initialize_deck(DECK)
-players_cards = deck.pop(2)
-dealers_cards = deck.pop(2)
-print_deal(players_cards, dealers_cards)
+prompt "Welcome to Twenty One!"
+prompt "First person to win #{WINNING_SCORE} games is the winner!"
+no_wins = { player: 0, dealer: 0, tie: 0 }
 loop do
-  break if calculate_score(players_cards) == 21
-  prompt "Do you want to hit (h) or stay (s)?"
-  reply = gets.chomp.downcase
-  break if reply == 's' || busted?(players_cards)
-  if reply == 'h'
-    new_card = deck.pop
-    prompt "You hit (#{face_value(new_card[1])})"
-    players_cards << new_card
-    sleep 2
-    print_deal(players_cards, dealers_cards)
-    break if busted?(players_cards)
-  else
-    prompt "Please select hit (h) or stay (s)..."
-  end
-end
-if busted?(players_cards)
-  prompt "You bust..."
-else
-  prompt "The dealer turns over his hidden card..."
   sleep 1
+  puts ""
+  prompt "Round #{no_wins.values.reduce(:+) + 1}"
+  puts ""
+  deck = initialize_deck(DECK)
+  players_cards = deck.pop(2)
+  dealers_cards = deck.pop(2)
+  player_scr = calculate_score(players_cards)
+  print_deal(players_cards, dealers_cards, player_scr)
   loop do
-    print_deal(players_cards, dealers_cards, false)
-    sleep 1
-    if calculate_score(dealers_cards) < 17
+    break if player_scr == GAME_MAX
+    prompt "Do you want to hit (h) or stay (s)?"
+    reply = gets.chomp.downcase
+    break if reply == 's' || busted?(player_scr)
+    if reply == 'h'
       new_card = deck.pop
-      prompt "Dealer hits (#{face_value(new_card[1])})"
-      dealers_cards << new_card
+      prompt "You hit (#{face_value(new_card[1])})"
+      players_cards << new_card
+      player_scr = calculate_score(players_cards)
       sleep 2
+      print_deal(players_cards, dealers_cards, player_scr)
+      break if busted?(player_scr)
+    else
+      prompt "Please select hit (h) or stay (s)..."
     end
-    break if busted?(dealers_cards) || calculate_score(dealers_cards) >= 17
   end
+  dealer_scr = calculate_score(dealers_cards)
+  if busted?(player_scr)
+    prompt "You bust..."
+  else
+    prompt "The dealer turns over his hidden card..."
+    sleep 1
+    loop do
+      print_deal(players_cards, dealers_cards, player_scr, dealer_scr, false)
+      sleep 1
+      if dealer_scr < DEALER_STAY
+        new_card = deck.pop
+        prompt "Dealer hits (#{face_value(new_card[1])})"
+        dealers_cards << new_card
+        dealer_scr = calculate_score(dealers_cards)
+        sleep 2
+      end
+      break if busted?(dealer_scr) || dealer_scr >= DEALER_STAY
+    end
+  end
+  if busted?(dealer_scr)
+    prompt "The dealer busts..."
+  end
+  winner = determine_winner(player_scr, dealer_scr)
+  no_wins[winner] += 1
+  print_summary(player_scr, dealer_scr, winner, no_wins)
+  break if no_wins.values.any? { |score| score == WINNING_SCORE }
 end
-if busted?(dealers_cards)
-  prompt "The dealer busts..."
-end
-puts determine_winner(players_cards, dealers_cards)
+puts ""
+prompt "#{no_wins.key(no_wins.values.max).to_s.capitalize} is the champion!"
+prompt "Thanks for playing Twenty One.  Goodbye!"
